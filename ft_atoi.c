aec32e9b34fc6d0cb7641c3684d7f586211de76b
Matthew Williams <ElCapitan@Matthews-iMac.local>
2016-12-07 00:44:45 +0200

    ft_atoi fixed

--- ft_atoi.c
+++ ft_atoi.c
@@ -10,37 +10,46 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-int		ft_atoi(char *s)
+#include "libft.h"
+
+static	int	get_start(char *s)
+{
+	int		cnt;
+
+	cnt = 0;
+	while (s[cnt] != '\0')
+	{
+		if (s[cnt] > 32 && s[cnt] < 127)
+			return (cnt);
+		cnt++;
+	}
+	return (-1);
+}
+
+int			ft_atoi(char *s)
 {
 	int		cnt;
 	int		out_int;
+	int		is_signed;
 
 	out_int = 0;
-	if (s[0] == '-')
-		cnt = 1;
-	else
-		cnt = 0;
-	while (s[cnt] && s[cnt] > 47 && s[cnt] < 58)
+	is_signed = 0;
+	cnt = get_start(s);
+	if (cnt == -1)
+		return (0);
+	if (s[cnt] == '-')
+	{
+		if (ft_isdigit(s[cnt + 1]) == 0)
+			return (0);
+		is_signed = 1;
+		cnt++;
+	}
+	while (ft_isdigit(s[cnt]) == 1)
 	{
 		out_int = out_int * 10 + (s[cnt] - '0');
 		cnt++;
 	}
-	if (s[0] == '-')
+	if (is_signed == 1)
 		out_int = -out_int;
 	return (out_int);
 }
-
-// int		ft_atoi(char *s)
-// {
-// 	int		cnt;
-// 	int		out_int;
-//
-// 	cnt = 0;
-// 	out_int = 0;
-// 	while (s[cnt])
-// 	{
-// 		out_int = out_int * 10 + (s[cnt] - '0');
-// 		cnt++;
-// 	}
-// 	return (out_int);
-// }
