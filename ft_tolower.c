566b0968423f004a07033a5daab582eb76adfc69
Matthew VAN WYK <mvanwyk@e5r1p9.jnb.42.fr>
2016-12-20 15:08:04 +0200

    fixed toupper and tolower

--- ft_tolower.c
+++ ft_tolower.c
@@ -10,32 +10,10 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-/*V2*/
-
-// char	ft_tolower(char c)
-// {
-// 	if (c < 'A' || c > 'Z')
-// 		return ((unsigned char)c);
-// 	else
-// 		return ((unsigned char)(c + 32));
-// }
-
-/*V1*/
-
-char	ft_tolower(int c)
+int	ft_tolower(int c)
 {
 	if (c < 65 || c > 90)
 		return (c);
 	else
 		return (c + 32);
 }
-
-/*ALT*/
-
-// char	ft_tolower(unsigned char c)
-// {
-// 	if (c < (unsigned int )65 || c > (unsigned int)90)
-// 		return (c);
-// 	else
-// 		return (c + (unsigned int)32);
-// }
